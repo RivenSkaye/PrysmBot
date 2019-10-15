24e63407b683237588d11e3038ad493fcdbfbacd
FokjeM <pretender0906@gmail.com>
2019-10-15 20:31:38 +0200

    Added some functions, trying to implement remote restart

--- Prysm.py
+++ Prysm.py
@@ -29,30 +29,47 @@ import discord.ext.commands
 with open("Prysm.json", "r") as prysmjson:
     base_info = json.load(prysmjson)
 guilds = base_info["Guilds"]
-client = discord.ext.commands.Bot(max_messages=0, fetch_offline_members=False, command_prefix=";")
+bot = discord.ext.commands.Bot(max_messages=0, fetch_offline_members=False, command_prefix=";")
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
+    for guild in bot.guilds:
         if str(guild.id) in guilds.keys():
             print("Guild found: %s" % guilds[str(guild.id)])
         else:
             print("New guild! %s" % guild.name)
             guilds[str(guild.id)] = guild.name
             print(guilds)
+    # Once the loop's done, we save all servers we're in now.
+    saveJSON("Prysm.json", base_info)
 
 # async def initMessage(guild, channel):
+@bot.event
+async def on_command_error(ctx, err):
+    if isinstance(err.original, discord.ext.commands.MissingPermissions):
+        ctx.channel.send("Sorry %s, it seems you lack the permission %s" % (ctx.author.mention(), ", ".join(err.original.missing_perms)))
 
-@client.command(name="exit", help="Calls all closing methods on the bot, shutting it down. Admins only!", pass_context=True)
+@bot.command(name="restart", help="Pulls in latest git code and restarts to load it in. Admins only!", pass_context=True)
+@discord.ext.commands.has_permissions(administrator=True)
+async def cmd_restart(ctx):
+    os.popen("git pull")
+    saveJSON("Prysm.json", base_info)
+    await bot.close()
+    os.execv("Prysm.py", [""])
+
+@bot.command(name="exit", help="Calls all closing methods on the bot, shutting it down. Admins only!", pass_context=True)
 @discord.ext.commands.has_permissions(administrator=True)
 async def cmd_exit(ctx):
     await ctx.channel.send("Prysm off, glad to be of service!")
     await bot_exit(0)
 
 async def bot_exit(status=0):
-    with open("Prysm.json", "w") as prysmjson:
-        json.dump(base_info, prysmjson, indent=4)
-    await client.close()
+    saveJSON("Prysm.json", base_info)
+    await bot.close()
     exit(status)
 
-client.run(base_info["Token"])
+def saveJSON(jsonFile, data):
+    with open(jsonFile, "w") as jsonHandle:
+        json.dump(data, jsonHandle, indent=4)
+
+bot.run(base_info["Token"])
