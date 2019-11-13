ed59af276d9b8a0cd86adaf4d41609e1b736c6d9
Riven Skaye <pretender0906@gmail.com>
2019-11-13 21:08:28 +0100

    Replaced Windows newlines, Python handles newline platform-independently

--- dep_install.py
+++ dep_install.py
@@ -15,5 +15,5 @@ for dep in dependencies:
     try:
         subprocess.Popen(["pip3", "install", "-U", dep]).wait()
     except Exception as e:
-        print("Error installing package: %s\r\n\tMessage: %s" % (dep, e))
+        print("Error installing package: %s\n\tMessage: %s" % (dep, e))
 print("Done installing dependencies; Please make sure you keep Prysm up to date!")
