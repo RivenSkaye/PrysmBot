a9613f8939af95d716ef55e26ba60e14151b0d9b
RivenSkaye <pretender0906@gmail.com>
2020-12-25 01:29:30 +0100

    rewrite in progress

--- Cogs/RSS.py
+++ Cogs/RSS.py
@@ -1,90 +1,80 @@
-"""
-RSS Reading cog for sending RSS feed information to Webhooks.
-This was built with the intent of sending stuff to Discord servers.
+""" RSS Reading cog for sending RSS feed information to Webhooks.
+
+This was built with the intent of sending stuff to Discord servers,
+leveraging the Webhook system to circumvent auth requirements, whilst
+allowing for users to separate the information per source visually and
+using names and identifiers of their choice.
+Webhooks given are parsed and checked to be actual Discord links and
+present in the server they're added from.
 Author: RivenSkaye
 Special thanks: Nala_Alan
                 The original code was his, rewritten to be callable from a
-                Discord bot. It has since been rewritten again to use his
-                base workflow, but heavily modified and with JSON requests
-                to the Discord WebHooks rather than HTTP form data
+                Discord bot. It has since been rewritten again, but maintains
+                his core parsing code with a small added check to require an
+                identifier field. Nala_Alan's POST request has been replaced
+                to send a JSON payload and use aiohttp in order to fetch and
+                send the data asynchronously, to be non-blocking to the bot's
+                other coroutines.
 """
-# No import for the cog system, this should just run without any commands.
+
 # Built-ins
 import asyncio
 import json
-from time import sleep # Used to prevent getting blocked for too many requests
+from typing import Optional, Union
+from datetime import datetime
 # Dependencies
-from discord.ext import commands
 import discord
+from discord.ext import commands
+import aiohttp
 import feedparser
-import requests
-from dateutil.parser import parse
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler import triggers
 
-"""
-Nala_Alan's keysort function.
-This does not belong in the class, but the code depends on it.
-"""
 def keysort(val):
+    """ Nala_Alan's keysort function. Meant to be used as a callback.
+
+    We only need a single key to sort on, which is why we can't use just the
+    built-in and instead need a simple wrapper func. Might a lambda be better?
+    This does not belong in the class, but the code depends on it.
+    """
     return parse(val["published"])
 
 class Prysm_RSS:
-    """
-    Simple Python class to complement Prysm.
+    """Simple Python class to complement Prysm. Users don't touch this class!
+
+    THIS WILL SOON BE DEPRECATED MOVED TO :class:`RSS`!!
+
     Fetches RSS feeds as defined in a file and sends back a message for all new
     entries in the feed.
     Primarily aimed at Nyaa releases, might gain more uses later.
     """
 
-    def __init__(self, rssfile: str="RSS.json", rsspath: str="./RSS/", rssarchive: str="archive.txt"):
-        if not self._check_create_file(filename=rssfile, filepath=rsspath):
-            print(f"Created a new file for info at {rsspath}{rssfile}")
-        if not self._check_create_file(filename=rssarchive, filepath=rsspath):
-            print(f"Created a new file for info at {rsspath}{rssfile}")
-        self.file = rssfile
+    def __init__(self, bot, rsspath: str="./RSS/"):
+        self.bot = bot
         self.path = rsspath
-        self.archive = rssarchive
 
-    """
-    Function to check if the files for sending feed info exist.
-    If they don't exist, they will be created.
+# MOVE ALL OF THIS CODE AND REWRITE FOR aiohttp
 
-    :param filename: str:   Name of the file to look for, defaults to 'RSS.json'.
-    :param filepath: str:   Path where the file is found. Relative or exact.
-                            Be aware of the CWD for relative paths!
-
-    :return: bool:          True if the file was there, false if not.
-    """
-    def _check_create_file(self, filename: str, filepath: str) -> bool:
-        retval = False # Assume it's not there, always
-        try:
-            with open(filepath+filename, "r") as rss: # Try opening it in read-only mode
-                retval = True # If it works, we flip to true
-        except FileNotFoundError: # If it didn't exist...
-            with open(filepath+filename, "w+") as rss: # ... create it and write to it
-                rss.write("") # Make it an empty JSON file.
-        return retval
-
-    """
-    Checks an RSS feed for new data and stores the ID for this feed in the archive.
-
-    :param feed: str:           URL to the actual RSS feed.
-    :param archive: str:        filename for the archive file. The path is assumed
-                                to be the same as where the RSS.json lives.
-    :param webhook: str:        URL to post the webhook messages to.
-    :param archive_only: bool:  Whether or not just to archive URLs without posting.
-                                Defaults to False, run with True the first time
-                                to prevent massive spam!
-    :param **kwargs: Any:       Keyword Args for the JSON data sent to Discord.
-                                content, file and payload_jason will be ignored!
-                                For more info, consult Discord's docs:
-                                https://discord.com/developers/docs/resources/webhook#execute-webhook
-
-    :return: int:               0 on success.
-                                1 if a feed doesn't work,
-                                2 if a webhook doesn't respond.
-                                3 means PANIC MODE.
-    """
     async def rss_fetch(self, feed: str, path: str, archive: str, webhook: str, archive_only: bool=False, **kwargs) -> int:
+        """Checks an RSS feed for new data and archives it
+
+        :param feed: str:           URL to the actual RSS feed.
+        :param archive: str:        filename for the archive file. The path is assumed
+                                    to be the same as where the RSS.json lives.
+        :param webhook: str:        URL to post the webhook messages to.
+        :param archive_only: bool:  Whether or not just to archive URLs without posting.
+                                    Defaults to False, run with True the first time
+                                    to prevent massive spam!
+        :param **kwargs: Any:       Keyword Args for the JSON data sent to Discord.
+                                    content, file and payload_jason will be ignored!
+                                    For more info, consult Discord's docs:
+                                    https://discord.com/developers/docs/resources/webhook#execute-webhook
+
+        :return: int:               0 on success.
+                                    1 if a feed doesn't work,
+                                    2 if a webhook doesn't respond.
+                                    3 means PANIC MODE.
+        """
         try:
             data = requests.get(url=feed, timeout=7.5).text # Timeout after 7.5s.
         except:
@@ -95,22 +85,25 @@ class Prysm_RSS:
                 old_archive = [val for val in arch.read().split('\n') if val]
             new_archive = []
             for release in sorted(feedparser.parse(data).entries, key=keysort):
+                relkeys = release.keys()
                 # Grab the correct identifier name, which is something like these
-                if "guid" in release.keys():
+                if "guid" in relkeys:
                     rel = release["guid"]
-                elif "GUID" in release.keys():
+                elif "GUID" in relkeys:
                     rel = release["GUID"]
-                elif "uid" in release.keys():
+                elif "uid" in relkeys:
                     rel = release["guid"]
-                elif "UID" in release.keys():
+                elif "UID" in relkeys:
                     rel = release["GUID"]
-                elif "id" in release.keys():
+                elif "id" in rrelkeys:
                     rel = release["id"]
-                elif "ID" in release.keys():
+                elif "ID" in relkeys:
                     rel = release["ID"]
                 else: # No identifier we can recognize? Just drop it and die.
                     print(f"The feed located at {feed} does not provide a known common identifier for entries.\nIt can therefore not be parsed reliably for new entries.\n\tIt has been deleted.")
                     return 1;
+                if "title" in relkeys:
+                    rel = f"**{release['title']}**\n{rel}"
                 if rel not in old_archive:
                     try:
                         if not archive_only:
@@ -133,19 +126,19 @@ class Prysm_RSS:
         # If due to a freak accident we got an empty string
         return 3
 
-    """
-    Loops over all feeds listed in the feeds file. Allows to set a limit to
-    limit the amount of requests made per second.
-
-    :param limit: int:          The limit on the amount of requests to send
-                                per second. Set to 0 to disable.
-                                Use this parameter to prevent sending too many
-                                requests to a feed and getting blocked.
-                                Defaults to 500 requests per second.
-    :param archive_only: bool:  Passed through to the singular rss_fetch.
-                                Defaults to False.
-    """
     async def fetch_all(self, limit: int=500, archive_only: bool=False):
+        """
+        Loops over all feeds listed in the feeds file. Allows to set a limit to
+        limit the amount of requests made per second.
+
+        :param limit: int:          The limit on the amount of requests to send
+                                    per second. Set to 0 to disable.
+                                    Use this parameter to prevent sending too many
+                                    requests to a feed and getting blocked.
+                                    Defaults to 500 requests per second.
+        :param archive_only: bool:  Passed through to the singular rss_fetch.
+                                    Defaults to False.
+        """
         with open(self.path+self.file) as rssjson:
             webhooks = json.load(rssjson)
         codes = []
@@ -156,7 +149,6 @@ class Prysm_RSS:
                 sleep(1/limit)
         for result in codes:
             code = await result
-            print(code)
             if code == 0:
                 continue
             if code == 1:
@@ -172,22 +164,130 @@ class RSS(commands.Cog):
     which Webhooks it should send the messages to.
     Also handles some setup for creating new Webhooks to work with.
     """
-    def __init__(self, bot):
-        self.bot = bot
-        self.COG_NAME = "RSS"
-        self.footer = "_RSS management is restricted to users that have both 'Manage Channel' and 'Manage Webhooks' permissions. Please keep this in mind when using commands in this cog._"
 
-    """
-    This is going to warn users that rss is not a command and then present them
-    with a nice list of all commands in this cog.
-    Calling this is basically giving out an error message!
-    """
-    @commands.group(invoke_without_command=True, cog_name="RSS", name="rss")
+    def __init__(self, bot, rsspath: str="./RSS/"):
+        self.bot = bot
+        self.path = rsspath
+        self.COG_NAME = "RSS"
+        self.footer = "_RSS management is restricted to bots and users that have the guild-wide 'Manage Channel' and 'Manage Webhooks' permissions._"
+
+    def _check_or_create_file(self, filename: str, filepath: str) -> bool:
+        """Function to check if a file exists, creates it if it doesn't.
+
+        :param filename: str:   Name of the file to look for, defaults to 'RSS.json'.
+        :param filepath: str:   Path where the file is found. Relative or exact.
+                                Be aware of the CWD for relative paths!
+
+        :return: bool:          True if the file was there, false if not.
+        """
+        retval = False # Assume it's not there, always
+        try:
+            # Try and see if the file exists and flip to True
+            with open(filepath+filename, "r") as rss:
+                retval = True
+        # If it doesn't exist, we get a FileNotFound
+        except FileNotFoundError:
+            # So we create it.
+            with open(filepath+filename, "w+") as rss:
+                # Force a write
+                rss.write("")
+        return retval
+
+    @commands.group(invoke_without_command=True, case_insensitive=True, cog_name="RSS", name="rss")
+    @commands.has_guild_permissions(manage_channels=True, manage_webhooks=True)
+    @commands.guild_only()
     async def rss(self, ctx):
-        embedtext = "You have tried to call a command group as a command, this doesn't work unfortunately.\nThere's a set list of commands in this group that you _can_ use however."
+        """
+        This is going to warn users that rss is not a command and then present them
+        with a nice list of all commands in this cog.
+        Calling this is basically giving out a simplified help message!
+        """
+        embedtext = "This command group has no base command unfortunately.\nThere's a set list of commands in this group that you _can_ use however. More info on these with `help rss <command>`"
         commandlist = ""
         for command in self.get_commands():
             commandlist += f"- {command.name}\n"
         e = discord.Embed(title="RSS Cog Info", type="rich", colour=discord.Colour.from_rgb(172, 43, 43), author=ctx.author, description=embedtext, footer=self.footer)
         e.add_field(name="Commands in this cog:", value=commandlist)
-        ctx.channel.send(embed=e)
+        ctx.send(embed=e)
+
+    @rss.command(name='serverwebhooks', aliases=['servhooks', 'swh'])
+    async def get_webhooks(self, ctx):
+        """
+        Get all webhooks registered for this server on the Discord end of things.
+        Easy to get info to register new listeners with.
+        """
+        with ctx.typing():
+            hooks = []
+            webhooks = await ctx.guild.webhooks()
+            if len(webhooks) == 0:
+                ctx.message.send("There are no webhooks registered for this server.\nPlease add one and try again.")
+            for webhook in webhooks:
+                hook = f"**{webhook.name}**. id: {webhook.id}, channel: {webhook.channel}.\n"
+                hooks.append(hook)
+            msgs = bot.split_messages(hooks)
+            for msg in msgs:
+                await ctx.send(msg)
+
+    @rss.command(name='listall')
+    async def listall(self, ctx):
+        """
+        List all feeds the bot is sending for every webhook.
+        Operation may take a while if a lot of data has to be processed,
+        thus we set the bot to a typing status until it's done.
+        """
+        webhooks = await ctx.guild.webhooks()
+        async with ctx.typing():
+            with open(self.path+self.file, 'rt') as rf:
+                hooks = json.load(rf)
+                guildhooks = []
+                for wh in webhooks:
+                    if wh.url in hooks.keys():
+                        guildhooks.append(wh)
+            await ctx.send("**Feeds being listened to in this guild per webhook:**")
+            feeds = []
+            for wh in guildhooks:
+                feeds.append(f"**{wh.name}** ({wh.channel}):\n{hooks[wh.url]}\n------------------")
+                msgs = bot.split_messages(feeds)
+                for msg in msgs:
+                    await ctx.send(msg)
+
+    @rss.command(name="addfeed", aliases=['new', 'af', 'add'])
+    async def addfeed(self, ctx, feed: str, hook: Union[str,int], interval: int=60):
+        """Add an RSS feed to listen to, and a webhook to send the messages to.
+
+        Minimum time is every 15 minutes, maximum is whatever apscheduler allows.
+
+        :param feed: str:       The url pointing to the RSS feed.
+                                May be enclosed in <> for link suppression.
+        :param hook: str:       For any Webhook in this guild: the id, URL, or name.
+        :param interval: int:   The interval between fetches of the feed.
+                                provide time in minutes, with a minimum of 15.
+                                Ignored if a crontab is given instead
+        :param fromtime: str:   either "now" or a timestamp in "hh:mm:ss" format.
+                                defaults to "now" if omitted
+        :param crontab: str:    A valid crontab to run the command on.
+                                Bound to the same 15 minute constraint as interval,
+                                it gets checked for this!
+        """
+
+        # Prepare an error message for malformed use of the command.
+        # The most probable issue is a malformed webhook identifier.
+        # Not an RSS feed is its own beast and will be handled later.
+        errcontent = f"""Could not add {feed} to the list due to a problem with the given Webhook.\n
+        Due to the nature of Webhooks in Discord, names are case sensitive, the IDs are series of 13 numbers and URLs are without spaces.\n
+        You can supply any one of these in the correct format to this function.\n\n
+
+        Please retry the command in the format `{ctx.prefix}{ctx.invoked_with} {feed} <Webhook name, ID, or URL> <interval in minutes>`."""
+        webhooks = await ctx.guild.webhooks()
+        for wh in webhooks:
+            if hook in [wh.id, wh.name, wh.url]:
+                target = wh.url
+                break
+        else:
+            e = self.bot._gen_err(command=ctx.invoked_with, content=errcontent, member=ctx.author)
+            await ctx.send(embed=e)
+            return
+        # We managed to find the Webhook, so now we set up shit to push to it.
+        date = datetime.now(datetime.timezone.utc)
+        # Create a trigger and make something to save to DB here
+        pass
--- Prysm.py
+++ Prysm.py
@@ -1,39 +1,113 @@
-#!/usr/bin/env python3
-
 """
 A utility bot for Discord.
-Functionality is currently very limited.
-Please forgive me as I expand the bot.
+This is the v2 rewrite, which uses actually proper code and tries to make it
+as sleek and lightweight as possible. Try not to use or run anything that isn't
+required and try not to use more resources than necessary.
 
-Author: RivenSkaye / FokjeM
+Generator functions are better than duplicating code, since they leave a smaller
+memory footprint and should be small, fast tasks that can be mostly asynced.
+
+Author: RivenSkaye
 """
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Union
 import os
-import subprocess
-import sys
 import json
-import math
-import importlib
+import asyncio
 # Discord libraries
 import discord
 import discord.ext.commands
 
-"""
-Utility fuction to save data to a JSON file.
-Mostly used to not duplicate lines of code all over.
+def _saveJSON(jsonFile: str, data: Dict[str, Any]):
+    """Utility fuction to save data to a JSON file.
 
-param: str jsonFile:    A file name or path for use with open()
-param: Dict data:        The data to be saved, dicts convert very easily.
-"""
-def saveJSON(jsonFile: str, data: Dict[str, Any]):
+    param: str jsonFile:    A file name or path for use with open()
+    param: Dict data:       The data to be saved, dicts convert very easily.
+    """
     with open(jsonFile, "w") as jsonHandle:
         json.dump(data, jsonHandle, indent=4)
+
+async def split_messages(list_in, limit: int=1750) -> List:
+    """Internal utility function to split messages before they get too big.
+    Leaves 250 chars of space to perform join and formatting operations.
+
+    :param list: list_in:   The input list to process
+    :param int: limit:      Max amount of chars in a message.
+                            Defaults to 1750 to be safe on the 2k limit.
+
+    :return: list:          A list of messages to send in succession
+    """
+    ret = []
+    msg = ""
+    for entry in list_in:
+        # append the messages until we get to the char limit
+        if len(msg) + len(str(entry)) <= limit:
+            msg.append(str(entry))
+        else:
+            ret = msg
+            msg = entry
+    # Make sure to add the last message
+    ret.append(msg)
+    return ret
+
+async def _gen_err(command: str, content: str, member: discord.Member) -> discord.Embed:
+    """Generator for error embeds, used throughout the bot.
+
+    Standardizes the color and format. Takes in the message body as a whole,
+    so generate this from the function that errors, or from the on_error handler
+    and try to be descriptive but brief.
+    Make sure to pass all important info to the user!
+    """
+    return discord.Embed(title=f"Error executing `{command}`!",
+                         description=content,
+                         colour=discord.Colour.from_rgb(172, 43, 43),
+                         author=member,
+                         footer="_Making mistakes is human. Seeing this message frequently just makes you **very** human._")
+
+def _gen_crontab(h: Union[int,str], m: Union[int,str], s: Union[int,str], freq: int=3, unit="h"):
+    """Generator for crontabs to be used with apscheduler.
+
+    Takes an input time split by hours minutes and seconds and returns a valid
+    crontab that repeats an action on every interval of frequency units.
+    If units is m (minutes), freq must be in the range 15-60.
+    If units is h (hours), freq must be in the range 1-24.
+    Does not raise anything, just assumes user inadequacy
+    """
+    crontab = {'hour': None, 'minute': None, 'second': None}
+    unit = unit[:1].lower()
+    if not unit in ['m', 'h']:
+        # User is inadequate, default to whatever will annoy them the most.
+        return {'month': "*/2", 'day': 31}
+    hours = []
+    mins = []
+    if unit == 'h':
+        h = h%freq
+        loop = 24/freq
+        while loop > 0:
+            hours.append(str(h))
+            h += freq
+            loop -= 1
+        crontab['hour'] = ",".join(hours)
+        crontab['minute'] = str(m)
+    else:
+        m = m%freq
+        loop = 60/freq
+        while loop > 0:
+            mins.append(str(h))
+            m += freq
+            loop -= 1
+        crontab['minute'] = ",".join(mins)
+        crontab['hour'] = str(h)
+    crontab['second'] = str(h)
+    return crontab
+
+# Create a class for the bot here, rather than a monolithic script
 
 try:
     with open("Prysm.json", "r") as prysmjson:
         base_info = json.load(prysmjson)
         assert (len(base_info["Token"]) > 0), "No token given! Fix your Prysm.json!"
 except FileNotFoundError:
+    # Create the Prysm.json file manually, ensure we have working stuff.
     with open("Prysm.json", "w+") as prysmjson:
         prysmjson.write("{\n    \"Token\": \"\",\n    \"Guilds\": {}\n}")
         print("There was no Prysm.json found! It was created, now add the Token for the bot.")
@@ -65,7 +139,7 @@ async def on_ready():
     for guild in bot.guilds:
         if str(guild.id) not in guilds.keys():
             guilds[str(guild.id)] = guild.name
-        if os.path.isfile(str("Guilds/"+str(guild.id)+".json")):
+        if os.path.isfile(str(f"Guilds/{str(guild.id)}.json")):
             with open(str("Guilds/"+str(guild.id)+".json")) as gjson:
                 g = json.load(gjson)
                 if "Init" in g:
